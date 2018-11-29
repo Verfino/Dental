be9b6a23a9f6ad3ebcfdcc2e8d5914d8e60248b6
AdrianMazgaj <adri9706@wp.pl>
2018-11-29 23:35:40 +0100

    Front view - alert dialog added for some action on click

--- app/src/main/java/com/amok/dental/MainActivity.java
+++ app/src/main/java/com/amok/dental/MainActivity.java
@@ -1,7 +1,13 @@
 package com.amok.dental;
 
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.content.DialogInterface;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ClickSettings(View view) {
+    }
+
+    public void ClickNextPage(View view) {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("NextPage");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 }
