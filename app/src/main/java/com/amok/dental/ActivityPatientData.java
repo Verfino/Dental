537e9849c8b2ffd221a8af7daf0e8ccc05b696c0
AdrianMazgaj <adri9706@wp.pl>
2018-12-08 10:15:36 +0100

    AutoCompleteTextView

--- app/src/main/java/com/amok/dental/ActivityPatientData.java
+++ app/src/main/java/com/amok/dental/ActivityPatientData.java
@@ -1,12 +1,16 @@
 package com.amok.dental;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.MultiAutoCompleteTextView;
 
 public class ActivityPatientData extends AppCompatActivity {
+
+
     //todo Check differences between multiAutoCompleteTextView and AutoCompleteTextView
     private static final String[] TREATMENTS = new String[] {
             "Leczenie kanalowe", "Mostek", "Korona", "Wyrywanie zeba","Leczenie zabkow"
@@ -20,5 +24,7 @@ public class ActivityPatientData extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, TREATMENTS);
         MultiAutoCompleteTextView textView = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView1);
         textView.setAdapter(adapter);
+        int a = getResources().getColor(R.color.colorCase0);
+        Log.d("STATE", "hahaha" + a);
     }
 }
